125df15598d8ee189e40ac2d88f8d7636db41104
dktrotti <dktrotti@ualberta.ca>
2016-01-30 16:40:09 -0700

    Added files

---
+++ app/src/main/java/dktrotti/transitr/GTFSReader.java
@@ -0,0 +1,11 @@
+package dktrotti.transitr;
+
+import com.squareup.wire.*;
+
+/**
+ * Reads GTFS data.
+ *
+ * Created by Dominic on 16-01-30.
+ */
+public class GTFSReader {
+}
---
+++ app/src/main/java/dktrotti/transitr/HTTPGrabber.java
@@ -0,0 +1,9 @@
+package dktrotti.transitr;
+
+/**
+ * Grabs live bus data from a server and saves the .pb file
+ *
+ * Created by Dominic on 16-01-30.
+ */
+public class HTTPGrabber {
+}
